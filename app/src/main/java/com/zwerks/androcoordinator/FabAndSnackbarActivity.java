25e2e66a79e5c66f3bbed01f05f13c4d777c69eb
Irvin Homem <irvinhomem@gmail.com>
2017-02-17 14:57:20 +0100

    Fixing onClick Listeners on "Show Snackbar button" and on Snackbar itself

--- app/src/main/java/com/zwerks/androcoordinator/FabAndSnackbarActivity.java
+++ app/src/main/java/com/zwerks/androcoordinator/FabAndSnackbarActivity.java
@@ -1,13 +1,39 @@
 package com.zwerks.androcoordinator;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FabAndSnackbarActivity extends AppCompatActivity {
+
+    private Button mShowSnackbarButton;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fab_and_snackbar);
+
+        // Assign the Coordinator Layour to a variable
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        // Assign showSnackBarButton to a variable
+        mShowSnackbarButton = (Button) findViewById(R.id.showSnackbarButton);
+
+        mShowSnackbarButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Snackbar.make(mCoordinatorLayout,
+                        "This is a simple Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("CLOSE", new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v){
+                                // Custom action
+                            }
+                        }).show();
+            }
+        });
     }
 }
