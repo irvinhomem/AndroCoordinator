3834bc826e8099a5fdad8224b22b5e9947410093
Irvin Homem <irvinhomem@gmail.com>
2017-02-20 17:56:50 +0100

    Fixed motion of the CircleImageView depending on (with respect to) the height (slide-in) of the Snackbar :-)

--- app/src/main/java/com/zwerks/androcoordinator/CustomBehaviour.java
+++ app/src/main/java/com/zwerks/androcoordinator/CustomBehaviour.java
@@ -2,6 +2,7 @@ package com.zwerks.androcoordinator;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -25,7 +26,8 @@ public class CustomBehaviour extends CoordinatorLayout.Behavior<CircleImageView>
     // The "layoutDependsOn" method is overridden  to specify which class of Views we want to watch out for
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, CircleImageView child, View dependency) {
-        return dependency instanceof Toolbar;
+        return dependency instanceof Toolbar || dependency instanceof Snackbar.SnackbarLayout;
+        //return dependency instanceof Toolbar;
     }
 
     // The "onDependentViewChanged" method is overridden so as to specify what should happen when the dependent view has changed
@@ -47,6 +49,29 @@ public class CustomBehaviour extends CoordinatorLayout.Behavior<CircleImageView>
             child.setScaleX(1+scale);
             child.setScaleY(1+scale);
         }
+
+        //Check for the current height of the Snackbar, and Translate the childView on the Y-axis by the height
+        //"Dependency" here is the Snackbar and "child" is the CircleImageView
+        if(dependency instanceof Snackbar.SnackbarLayout){
+            //Log.d(TAG, "Snackbar Height: " + String.valueOf(dependency.getHeight()));
+            //Log.d(TAG, "Snackbar Top: " + String.valueOf(dependency.getTop()));
+            Log.d(TAG, "Snackbar TranslationY: " + String.valueOf(dependency.getTranslationY()));
+            Log.d(TAG, "Snackbar Y-location: " + String.valueOf(dependency.getY()));
+
+            float newPosition = dependency.getTranslationY();
+            Log.d(TAG, "New POSITION: " + String.valueOf(newPosition));
+            //child.setY(dependency.getY());
+            //child.setTranslationY(dependency.getTranslationY());          // <-- Somewhat work
+            //child.setTranslationY(dependency.getTranslationY()-60);          // <-- Somewhat work
+            child.setTranslationY(dependency.getTranslationY() - dependency.getHeight());          // <-- WORKS !!!!!!! Finally!
+            //child.setTranslationY(dependency.getTranslationY() + (dependency.getY() - child.getY()));  //<-- Somewhat works
+            //child.setY(dependency.getTranslationY() + (dependency.getY() - child.getY()));  //<-- Somewhat also works
+
+            //child.setTranslationY(dependency.getTranslationY() + dependency.getHeight());
+            //child.setTranslationY(dependency.getTranslationY() + dependency.getMeasuredHeight());
+            //child.setY(child.getY() - (dependency.getY() - child.getY()));
+        }
+
         return false;
     }
 }
